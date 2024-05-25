8ccd25554fb991e5373fc1be503adf13fdb6b023
CharithAnupama58 <charith20anupama@gmail.com>
2024-05-25 10:34:37 +0530

    Validations

--- Client/src/components/addStock.jsx
+++ Client/src/components/addStock.jsx
@@ -1,53 +1,71 @@
-import  { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
-
 
 const AddStock = () => {
     const [options, setOptions] = useState([]);
     const [itemNameDetails, setItemNameDetails] = useState(null);
     const [selectedOption, setSelectedOption] = useState('');
     const [ItemId, setItemId] = useState('');
-    let [Quantity, setStockQty] = useState('');
-    const [ExpiryDate, setExpDate] = useState([]);
+    const [Quantity, setStockQty] = useState('');
+    const [ExpiryDate, setExpDate] = useState('');
+    const [errors, setErrors] = useState({});
+    const [isFormValid, setIsFormValid] = useState(false);
 
     const fetchOptions = async () => {
         try {
-          const response = await axios.get('http://localhost:3001/server/stockManagement/itemIds'); 
-          setOptions(response.data.options);
+            const response = await axios.get('http://localhost:3001/server/stockManagement/itemIds');
+            setOptions(response.data.options);
         } catch (error) {
-          console.error('Error fetching options:', error);
+            console.error('Error fetching options:', error);
         }
-      };
-      
+    };
+
     const fetchItemDetails = async () => {
         try {
             const response = await axios.get(`http://localhost:3001/server/stockManagement/itemNameDetails/${selectedOption}`);
             setItemNameDetails(response.data.itemNameDetails[0]);
-
+            setItemId(response.data.itemNameDetails[0].itemId);
         } catch (error) {
             console.error('Error fetching item details:', error);
         }
     };
+
     useEffect(() => {
         fetchOptions();
-
         if (selectedOption) {
             fetchItemDetails();
-        }  
-         
-    },[selectedOption]);
+        }
+    }, [selectedOption]);
 
     const handleSelectChange = (event) => {
         setSelectedOption(event.target.value);
+        validateForm(event.target.value, Quantity, ExpiryDate);
     };
-    const handleInputChanges = () => {
-        setItemId(document.getElementById('input1').value);
-        setStockQty(document.getElementById('input4').value);
-        setExpDate(document.getElementById('input5').value);
-    };  
+
+    const handleInputChanges = (event) => {
+        const { id, value } = event.target;
+        if (id === 'input4') setStockQty(value);
+        if (id === 'input5') setExpDate(value);
+        validateForm(selectedOption, id === 'input4' ? value : Quantity, id === 'input5' ? value : ExpiryDate);
+    };
+
+    const validateForm = (selectedOption, Quantity, ExpiryDate) => {
+        const newErrors = {};
+        const quantityPattern = /^[1-9]\d*$/;
+        const datePattern = /^\d{4}-\d{2}-\d{2}$/;
+
+        if (!selectedOption) newErrors.selectedOption = true;
+        if (!Quantity || !quantityPattern.test(Quantity)) newErrors.Quantity = true;
+        if (!ExpiryDate || !datePattern.test(ExpiryDate)) newErrors.ExpiryDate = true;
+
+        setErrors(newErrors);
+        setIsFormValid(Object.keys(newErrors).length === 0);
+    };
+
     const handleAddStockSubmit = async (e) => {
         e.preventDefault();
-        console.log(ItemId, Quantity, ExpiryDate);
+        if (!isFormValid) return;
+
         try {
             const response = await axios.post('http://localhost:3001/server/stockManagement/addStock', {
                 ItemId,
@@ -56,66 +74,108 @@ const AddStock = () => {
             });
 
             if (response.status === 200) {
-                alert('Feedback submitted successfully');
+                alert('Stock added successfully');
                 setItemId('');
+                setStockQty('');
+                setExpDate('');
+                setSelectedOption('');
+                setItemNameDetails(null);
             } else {
-                console.error('Invalid username or password');
+                console.error('Failed to add stock:', response.data.message);
             }
         } catch (error) {
-            console.error('Login failed:', error);
-            console.error('Failed to login. Please try again later.');
+            console.error('Failed to add stock:', error);
         }
     };
-    
+
     return (
         <div className='flex flex-row w-full justify-center'>
             <div className='flex flex-col items-center'>
-                        <h1 className='mt-10 font-bold text-4xl'>Add Stock</h1>
-                        <form className="ml-4 mt-16 w-96" onSubmit={handleAddStockSubmit}>
-                            <div className="mb-8 flex items-center">
-                                <label htmlFor="combo-box" className="block text-xl w-60 mr-4">Item Name:</label>
-                                <select id="combo-box" className="w-full h-10 px-3 rounded border border-black" value={selectedOption} onChange={handleSelectChange}>
-                                    <option value="">Select an option...</option>
-                                    {options.map((option, index) => (
-                                        <option key={index}>{option.itemName}</option>
-                                    ))}
-                                </select>
-                            </div>
-                            
-                                <div  className="mb-8 flex items-center">
-                                    <label htmlFor="input1" className="block text-xl w-60 mr-4">Item ID:</label>
-                                    <input type="text" id="input1" className="w-full h-10 px-3 rounded border border-black" placeholder="Item ID" value={itemNameDetails?.itemId||''} onChange={handleInputChanges} disabled/>
-                                </div>
-                      
-                            
-                                <div  className="mb-8 flex items-center">
-                                    <label htmlFor="input2" className="block text-xl w-60 mr-4">Measure Unit:</label>
-                                    <input type="text" id="input2" className="w-full h-10 px-3 rounded border border-black" placeholder="Measure Unit" value={itemNameDetails?.measureUnit||''} disabled />
-                                </div>
-                           
-                         
-                                <div  className="mb-8 flex items-center">
-                                    <label htmlFor="input3" className="block text-xl w-60 mr-4">Unit Qty:</label>
-                                    <input type="text" id="input3" className="w-full h-10 px-3 rounded border border-black" placeholder="Unit Quantity" value={itemNameDetails?.unitQty||''} disabled />
-                                </div>
-                           
-                            
-                            <div className="mb-8 flex items-center">
-                                <label htmlFor="input4" className="block text-xl w-60 mr-4">Stock Qty:</label>
-                                <input type="number" id="input4" className="w-full h-10 px-3 rounded border border-black" placeholder="Stock Quantity" value={Quantity} onChange={handleInputChanges}  />
-                            </div>
-                            
-                            
-                            <div  className="mb-8 flex items-center">
-                                <label htmlFor="input5" className="block text-xl w-60 mr-4">Expiory Date:</label>
-                                <input type="date" id="input5" className="w-full h-10 px-3 rounded border border-black" placeholder="Exp Date" value={ExpiryDate} onChange={handleInputChanges} />
-                            </div>
-                
-                            <button className=' bg-custom-blue text-white font-bold rounded-xl mt-8 ml-20 py-1 px-6'>Add Stock</button>
-                            
-                        </form>
+                <h1 className='mt-10 font-bold text-4xl'>Add Stock</h1>
+                <form className="ml-4 mt-16 w-96" onSubmit={handleAddStockSubmit}>
+                    <div className="mb-8 flex items-center">
+                        <label htmlFor="combo-box" className="block text-xl w-60 mr-4">Item Name:</label>
+                        <select
+                            id="combo-box"
+                            className={`w-full h-10 px-3 rounded border ${errors.selectedOption ? 'border-red-500' : selectedOption ? 'border-green-500' : 'border-gray-500'}`}
+                            value={selectedOption}
+                            onChange={handleSelectChange}
+                        >
+                            <option value="">Select an option...</option>
+                            {options.map((option, index) => (
+                                <option key={index} value={option.itemName}>{option.itemName}</option>
+                            ))}
+                        </select>
                     </div>
-            
+
+                    <div className="mb-8 flex items-center">
+                        <label htmlFor="input1" className="block text-xl w-60 mr-4">Item ID:</label>
+                        <input
+                            type="text"
+                            id="input1"
+                            className="w-full h-10 px-3 rounded border border-gray-500"
+                            placeholder="Item ID"
+                            value={itemNameDetails?.itemId || ''}
+                            disabled
+                        />
+                    </div>
+
+                    <div className="mb-8 flex items-center">
+                        <label htmlFor="input2" className="block text-xl w-60 mr-4">Measure Unit:</label>
+                        <input
+                            type="text"
+                            id="input2"
+                            className="w-full h-10 px-3 rounded border border-gray-500"
+                            placeholder="Measure Unit"
+                            value={itemNameDetails?.measureUnit || ''}
+                            disabled
+                        />
+                    </div>
+
+                    <div className="mb-8 flex items-center">
+                        <label htmlFor="input3" className="block text-xl w-60 mr-4">Unit Qty:</label>
+                        <input
+                            type="text"
+                            id="input3"
+                            className="w-full h-10 px-3 rounded border border-gray-500"
+                            placeholder="Unit Quantity"
+                            value={itemNameDetails?.unitQty || ''}
+                            disabled
+                        />
+                    </div>
+
+                    <div className="mb-8 flex items-center">
+                        <label htmlFor="input4" className="block text-xl w-60 mr-4">Stock Qty:</label>
+                        <input
+                            type="number"
+                            id="input4"
+                            className={`w-full h-10 px-3 rounded border ${errors.Quantity ? 'border-red-500' : Quantity ? 'border-green-500' : 'border-gray-500'}`}
+                            placeholder="Stock Quantity"
+                            value={Quantity}
+                            onChange={handleInputChanges}
+                        />
+                    </div>
+
+                    <div className="mb-8 flex items-center">
+                        <label htmlFor="input5" className="block text-xl w-60 mr-4">Expiry Date:</label>
+                        <input
+                            type="date"
+                            id="input5"
+                            className={`w-full h-10 px-3 rounded border ${errors.ExpiryDate ? 'border-red-500' : ExpiryDate ? 'border-green-500' : 'border-gray-500'}`}
+                            placeholder="Expiry Date"
+                            value={ExpiryDate}
+                            onChange={handleInputChanges}
+                        />
+                    </div>
+
+                    <button
+                        className='bg-custom-blue text-white font-bold rounded-xl mt-8 ml-20 py-1 px-6'
+                        disabled={!isFormValid}
+                    >
+                        Add Stock
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
