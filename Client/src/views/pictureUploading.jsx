019068f262fdce4368090147e1236ca740dea4d5
CharithAnupama58 <charith20anupama@gmail.com>
2024-05-31 17:53:53 +0530

    Updated

--- Client/src/views/pictureUploading.jsx
+++ Client/src/views/pictureUploading.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import arrowLeft from '../assets/images/arrowLeft.png'
-import radiobtn from '../assets/images/Group 127.png'
-import radiobtn1 from '../assets/images/Group 127 (1).png'
-import imageBox from '../assets/images/imageBox.png'
-import { Link,useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import arrowLeft from '../assets/images/arrowLeft.png';
+import radiobtn from '../assets/images/Group 127.png';
+import radiobtn1 from '../assets/images/Group 127 (1).png';
+import imageBox from '../assets/images/imageBox.png';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import Swal from 'sweetalert2';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 export const PictureUploading = () => {
     const [dragging, setDragging] = useState(false);
     const [imageUrl, setImageUrl] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [showModal, setShowModal] = useState(false);
     const navigate = useNavigate();
 
-    // Prevent default drag behaviors
+   
     const handleDragOver = (e) => {
-      e.preventDefault();
-      setDragging(true);
+        e.preventDefault();
+        setDragging(true);
     };
+
   
-    // Handle file drop
     const handleDrop = async (e) => {
         e.preventDefault();
         setDragging(false);
-        const file = e.dataTransfer.files[0]
-        console.log(file); // Get the dropped file
+        const file = e.dataTransfer.files[0];
+        console.log(file); 
 
-        // Create FormData object
+       
         const formData = new FormData();
         formData.append('image', file);
 
         try {
+            setLoading(true);
             const response = await axios.post('http://localhost:3001/uploads', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
@@ -38,55 +43,97 @@ export const PictureUploading = () => {
             const url = response.data.imageUrl;
             setImageUrl(url);
             console.log(url);
-            alert('Image uploaded successfully. Link: ' + response.data.imageUrl);
+            Swal.fire({
+                icon: 'success',
+                title: 'Image Uploaded Successfully',
+                text: 'Your image has been uploaded.',
+            });
+            setShowModal(true);
         } catch (error) {
-            
             console.error('Error uploading image:', error);
-            alert('Error uploading image. Please try again.');
+            Swal.fire({
+                icon: 'error',
+                title: 'Upload Failed',
+                text: 'There was an error uploading the image. Please try again.',
+            });
+        } finally {
+            setLoading(false);
         }
     };
-    const handleNextClick = () => {
-        console.log(imageUrl);
-        const encodedUrl = encodeURIComponent(imageUrl);
-        navigate(`/PictureUploading2/${encodedUrl}`);
-    }
-  return (
-    <div className='flex h-screen w-screen justify-between'>
-        <Link to='/'><img src={arrowLeft} className='w-8 h-8 ml-8 mt-10' alt='Icon'></img></Link>
-        <div className='flex flex-col w-full mt-6'>
-            <div className='flex items-center justify-center'>
-                <h1 className='text-6xl font-bold'>Order Online</h1>
-            </div>
-            <div className='flex flex-row justify-between'>
-                <Link to='/CustomizeCake'><div className='flex flex-row'>
-                    <img src={radiobtn1} className='w-8 h-8 ml-32 mt-12'></img>
-                    <h1 className='ml-6 text-4xl mt-10 font-semibold'>Customize Cake</h1>
-                </div></Link>
-                <Link to='/PictureUploading'><div className='flex flex-row mr-36'>
-                    <img src={radiobtn} className='w-8 h-8 ml-32 mt-12'></img>
-                    <h1 className='ml-6 text-4xl mt-10 font-semibold'>Picture Uploading</h1>
-                </div></Link>
-            </div>
-            <div className='flex flex-col mt-10 '>
-                <h1 className='flex flex-row text-4xl font-semibold justify-center'>Cake Details</h1>
 
-                <div
-                    className={`image-drop-zone ${dragging ? 'dragging' : ''} flex border-4 items-center border-black w-1/2 mx-auto mt-24 rounded-3xl`}
-                    onDragOver={handleDragOver}
-                    onDragLeave={() => setDragging(false)}
-                    onDrop={handleDrop}
+    const handleNextClick = () => {
+        if (imageUrl) {
+            console.log(imageUrl);
+            const encodedUrl = encodeURIComponent(imageUrl);
+            navigate(`/PictureUploading2/${encodedUrl}`);
+        }
+    };
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    return (
+        <div className='flex h-screen w-screen justify-between'>
+            <Link to='/'><img src={arrowLeft} className='w-8 h-8 ml-8 mt-10' alt='Icon'></img></Link>
+            <div className='flex flex-col w-full mt-6'>
+                <div className='flex items-center justify-center'>
+                    <h1 className='text-6xl font-bold'>Order Online</h1>
+                </div>
+                <div className='flex flex-row justify-between'>
+                    <Link to='/CustomizeCake'>
+                        <div className='flex flex-row'>
+                            <img src={radiobtn1} className='w-8 h-8 ml-32 mt-12'></img>
+                            <h1 className='ml-6 text-4xl mt-10 font-semibold'>Customize Cake</h1>
+                        </div>
+                    </Link>
+                    <Link to='/PictureUploading'>
+                        <div className='flex flex-row mr-36'>
+                            <img src={radiobtn} className='w-8 h-8 ml-32 mt-12'></img>
+                            <h1 className='ml-6 text-4xl mt-10 font-semibold'>Picture Uploading</h1>
+                        </div>
+                    </Link>
+                </div>
+                <div className='flex flex-col mt-10'>
+                    <h1 className='flex flex-row text-4xl font-semibold justify-center'>Cake Details</h1>
+
+                    <div
+                        className={`image-drop-zone ${dragging ? 'dragging' : ''} flex border-4 items-center border-black w-1/2 mx-auto mt-24 rounded-3xl`}
+                        onDragOver={handleDragOver}
+                        onDragLeave={() => setDragging(false)}
+                        onDrop={handleDrop}
                     >
-                    <img src={imageBox}></img>
-                    {dragging ? <p className='flex ml-24 text-3xl font-semibold'>Drop the image here</p> : <p className='flex ml-24 text-3xl font-semibold'>Drag and drop an image here</p>}
-                    
+                        <img src={imageBox} alt='Drop Zone' />
+                        {dragging ? <p className='flex ml-24 text-3xl font-semibold'>Drop the image here</p> : <p className='flex ml-24 text-3xl font-semibold'>Drag and drop an image here</p>}
+                    </div>
+                    {loading && (
+                        <div className='flex justify-center mt-6'>
+                            <ClipLoader size={50} color={"#123abc"} loading={loading} />
+                        </div>
+                    )}
+                    <div className='flex mt-24 justify-center'>
+                        <button
+                            className={`text-xl w-36 bg-blue-500 text-white rounded-xl ${!imageUrl ? 'opacity-50 cursor-not-allowed' : ''}`}
+                            onClick={handleNextClick}
+                            disabled={!imageUrl}
+                        >
+                            Next
+                        </button>
+                    </div>
                 </div>
-                <div className='flex mt-24 justify-center'>
-                    <button className='text-xl w-36 bg-blue-500 text-white rounded-xl'onClick={handleNextClick}>Next</button>
+            </div>
+
+            {showModal && (
+                <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-50'>
+                    <div className='bg-white p-4 rounded-lg'>
+                        <h2 className='text-2xl font-bold mb-4'>Uploaded Image</h2>
+                        <img src={imageUrl} alt='Uploaded' className='max-w-full max-h-96' />
+                        <button className='mt-4 bg-blue-500 text-white px-4 py-2 rounded' onClick={handleCloseModal}>Close</button>
+                    </div>
                 </div>
-                
-            </div>   
+            )}
         </div>
-    </div>
-  )
-}
+    );
+};
+
 export default PictureUploading;
--- Client/src/views/pictureUploading2.jsx
+++ Client/src/views/pictureUploading2.jsx
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react'
-import arrowLeft from '../assets/images/arrowLeft.png'
-import radiobtn from '../assets/images/Group 127.png'
-import radiobtn1 from '../assets/images/Group 127 (1).png'
-import { Link,useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import arrowLeft from '../assets/images/arrowLeft.png';
+import radiobtn from '../assets/images/Group 127.png';
+import radiobtn1 from '../assets/images/Group 127 (1).png';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
+import Swal from 'sweetalert2';
+import { jsPDF } from 'jspdf';
 
 export const PictureUploading2 = () => {
-    const {imageUrl} =useParams();
+    const { imageUrl } = useParams();
     const [options, setOptions] = useState([]);
     const [selectedOption, setSelectedOption] = useState('');
     const [Name, setName] = useState('');
@@ -15,25 +17,25 @@ export const PictureUploading2 = () => {
     const [PickupDate, setPickupDate] = useState('');
     const [additionalText, setAdditionalText] = useState('');
     const [branchID, setBranchID] = useState('');
-    
+    const [isFormValid, setIsFormValid] = useState(false);
 
-// hhh
     const fetchOptions = async () => {
         try {
-          const response = await axios.get('http://localhost:3001/server/customizeCake/branchIds'); 
-          setOptions(response.data.options);
-        //   console.log(response.data.options);
+            const response = await axios.get('http://localhost:3001/server/customizeCake/branchIds');
+            setOptions(response.data.options);
         } catch (error) {
-          console.error('Error fetching options:', error);
+            console.error('Error fetching options:', error);
         }
-      };
-      useEffect(() => {
+    };
+
+    useEffect(() => {
         fetchOptions();
         console.log(imageUrl);
+    }, []);
 
-        
-         
-    },[selectedOption]);
+    useEffect(() => {
+        validateForm();
+    }, [Name, Contact, Quantity, PickupDate, selectedOption]);
 
     const handleSelectChange = (event) => {
         setSelectedOption(event.target.value);
@@ -43,27 +45,73 @@ export const PictureUploading2 = () => {
             setBranchID(selectedBranchId);
         }
     };
+
     const handlePickupChange = (event) => {
-        setPickupDate(event.target.value);
+        const selectedDate = new Date(event.target.value);
+        const today = new Date();
+        const minDate = new Date(today.setDate(today.getDate() + 4));
+        if (selectedDate >= minDate) {
+            setPickupDate(event.target.value);
+        } else {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Invalid Date',
+                text: 'Please select a date at least 4 days from today.',
+            });
+        }
     };
+
     const handleNameChange = (event) => {
         setName(event.target.value);
     };
+
     const handleContactChange = (event) => {
-        setContact(event.target.value);
+        const contact = event.target.value;
+        setContact(contact);
     };
+
     const handleQuantityChange = (event) => {
-        setQuantity(event.target.value);
+        const quantity = event.target.value;
+        if (/^\d{0,2}$/.test(quantity)) {
+            setQuantity(quantity);
+        }
     };
+
     const handleAdditionalTextChange = (event) => {
         setAdditionalText(event.target.value);
+    };
+
+    const validateForm = () => {
+        const isNameValid = Name.trim() !== '' && Name.length <= 50;
+        const isContactValid = /^\d{10}$/.test(Contact);
+        const isQuantityValid = Quantity.trim() !== '' && /^\d{1,2}$/.test(Quantity);
+        const isPickupDateValid = PickupDate.trim() !== '' && validatePickupDate(PickupDate);
+        const isBranchSelected = selectedOption.trim() !== '';
+
+        setIsFormValid(isNameValid && isContactValid && isQuantityValid && isPickupDateValid && isBranchSelected);
+    };
+
+    const validatePickupDate = (date) => {
+        const selectedDate = new Date(date);
+        const today = new Date();
+        const minDate = new Date(today.setDate(today.getDate() + 4));
+
+        if (selectedDate < minDate) {
+            Swal.fire({
+                icon: 'warning',
+                title: 'Invalid Pickup Date',
+                text: 'Pickup date must be at least 4 days from today.',
+            });
+            return false;
+        }
+        return true;
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const today = new Date();
         const formattedDate = today.toISOString().split('T')[0];
-        console.log(imageUrl,PickupDate,Name,Contact,selectedOption,Quantity,formattedDate,additionalText,branchID);
+        console.log(imageUrl, PickupDate, Name, Contact, selectedOption, Quantity, formattedDate, additionalText, branchID);
         try {
             const response = await axios.post('http://localhost:3001/server/pictureUploading/placePictureUploadingOrder', {
                 Name,
@@ -77,75 +125,123 @@ export const PictureUploading2 = () => {
             });
 
             if (response.status === 200) {
-                alert('Feedback submitted successfully');
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Order Submitted',
+                    text: `Your order ID is ${response.data.orderId}.`,
+                });
                 setName('');
                 setContact('');
                 setQuantity('');
                 setPickupDate('');
                 setSelectedOption('');
-                // navigate(`/CustomizeCake2/${cakeId}/${additionalText}`);
+                generatePDF(response.data.orderId);
             } else {
-                console.error('Invalid username or password');
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Order Submission Failed',
+                    text: 'Please try again later.',
+                });
             }
         } catch (error) {
-            console.error('Login failed:', error);
-            console.error('Failed to login. Please try again later.');
+            Swal.fire({
+                icon: 'error',
+                title: 'Order Submission Failed',
+                text: 'Please try again later.',
+            });
+            console.error('Submission failed:', error);
         }
     };
-  return (
-    <div className='flex h-screen w-screen justify-between'>
-        <Link to='/PictureUploading'><img src={arrowLeft} className='w-8 h-8 ml-8 mt-10' alt='Icon'></img></Link>
-        <div className='flex flex-col w-full mt-6'>
-            <div className='flex items-center justify-center'>
-                <h1 className='text-6xl font-bold'>Order Online</h1>
-            </div>
-            <div className='flex flex-row justify-between'>
-                <Link to='/CustomizeCake'><div className='flex flex-row'>
-                    <img src={radiobtn1} className='w-8 h-8 ml-32 mt-12'></img>
-                    <h1 className='ml-6 text-4xl mt-10 font-semibold'>Customize Cake</h1>
-                </div></Link>
-                <Link to='/PictureUploading'><div className='flex flex-row mr-36'>
-                    <img src={radiobtn} className='w-8 h-8 ml-32 mt-12'></img>
-                    <h1 className='ml-6 text-4xl mt-10 font-semibold'>Picture Uploading</h1>
-                </div></Link>
-            </div>
-            <div className='flex flex-col mt-10 '>
-                <h1 className='flex flex-row text-4xl font-semibold justify-center'>Contact Details</h1>
+
+    const generatePDF = (orderId) => {
+        const doc = new jsPDF();
+        
+        const pageWidth = doc.internal.pageSize.getWidth();
+        const title = 'Order Details';
+        const titleX = (pageWidth - doc.getTextWidth(title)) / 2;
+        doc.setFontSize(20);
+        doc.text(title, titleX, 20);
+        
+        doc.setFontSize(12);
+        const leftMargin = 20;
+        let yOffset = 40;
+        const lineSpacing = 10;
+        
+        const orderDetails = [
+            `Order ID: ${orderId}`,
+            `Name: ${Name}`,
+            `Contact: ${Contact}`,
+            `Quantity: ${Quantity}`,
+            `Pickup Date: ${PickupDate}`,
+            `Branch: ${selectedOption}`,
+            
+        ];
+        
+        orderDetails.forEach(detail => {
+            doc.text(detail, leftMargin, yOffset);
+            yOffset += lineSpacing;
+        });
+        
+        doc.save('order_details.pdf');
+    };
+
+    
+
+    return (
+        <div className='flex h-screen w-screen justify-between'>
+            <Link to='/PictureUploading'><img src={arrowLeft} className='w-8 h-8 ml-8 mt-10' alt='Icon'></img></Link>
+            <div className='flex flex-col w-full mt-6'>
+                <div className='flex items-center justify-center'>
+                    <h1 className='text-6xl font-bold'>Order Online</h1>
+                </div>
                 <div className='flex flex-row justify-between'>
-                    <div className='flex flex-row'>
-                        <input className='mt-12 w-96 mr-36 h-12 ml-36 border-2 border-black rounded-xl' type='Date' placeholder='Select Pickup Date' value={PickupDate} onChange={handlePickupChange}  />
-                    </div>
-                    <div className='flex flex-row'>
-                        <select id="combo-box" className="w-96 mt-14 ml-36 mr-36 h-10 px-3 rounded border-2 border-black" value={selectedOption} onChange={handleSelectChange}>
+                    <Link to='/CustomizeCake'><div className='flex flex-row'>
+                        <img src={radiobtn1} className='w-8 h-8 ml-32 mt-12'></img>
+                        <h1 className='ml-6 text-4xl mt-10 font-semibold'>Customize Cake</h1>
+                    </div></Link>
+                    <Link to='/PictureUploading'><div className='flex flex-row mr-36'>
+                        <img src={radiobtn} className='w-8 h-8 ml-32 mt-12'></img>
+                        <h1 className='ml-6 text-4xl mt-10 font-semibold'>Picture Uploading</h1>
+                    </div></Link>
+                </div>
+                <div className='flex flex-col mt-10 '>
+                    <h1 className='flex flex-row text-4xl font-semibold justify-center'>Contact Details</h1>
+                    <div className='flex flex-row justify-between'>
+                        <div className='flex flex-row'>
+                            <input className='mt-12 w-96 mr-36 h-12 ml-36 border-2 border-black rounded-xl' type='date' value={PickupDate} onChange={handlePickupChange} />
+                        </div>
+                        <div className='flex flex-row'>
+                            <select id="combo-box" className="w-96 mt-14 ml-36 mr-36 h-10 px-3 rounded border-2 border-black" value={selectedOption} onChange={handleSelectChange}>
                                 <option value="">Select a Branch</option>
                                 {options.map((option, index) => (
                                     <option key={index}>{option.branchName}</option>
                                 ))}
-                        </select>
+                            </select>
+                        </div>
                     </div>
+                    <form onSubmit={handleSubmit}>
+                        <div className='flex flex-row justify-between'>
+                            <div className='flex flex-row'>
+                                <input className='mt-12 w-96 mr-36 h-12 ml-36 border-2 border-black rounded-xl' type='text' placeholder='Add A text to add on the Cake' value={additionalText} onChange={handleAdditionalTextChange} maxLength="100" />
+                            </div>
+                            <div className='flex flex-row'>
+                                <input className='mt-12 w-96 ml-8 mr-36 border-2 border-black rounded-xl' type='text' placeholder='Contact' value={Contact} onChange={handleContactChange} maxLength="15" />
+                            </div>
+                        </div>
+                        <div className='flex flex-row justify-between'>
+                            <div className='flex flex-row'>
+                                <input className='mt-12 w-96 h-12 ml-36 border-2 border-black rounded-xl' type='text' placeholder='Name Of the Owner' value={Name} onChange={handleNameChange} maxLength="20" />
+                            </div>
+                            <div className='flex flex-col mt-6'>
+                                <input className='mt-6 w-96 mr-36 h-12 ml-36 border-2 border-black rounded-xl' type='text' placeholder='Quantity' value={Quantity} onChange={handleQuantityChange} maxLength="4" />
+                                <button type='submit' className='mt-16 ml-36 w-36 bg-blue-500 text-white rounded-xl' disabled={!isFormValid}>Submit</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <form onSubmit={handleSubmit}>
-                <div className='flex flex-row justify-between'>
-                    <div className='flex flex-row'>
-                        <input className='mt-12 w-96 mr-36 h-12 ml-36 border-2 border-black rounded-xl' type='text' placeholder='Add A text to add on the Cake' value={additionalText} onChange={handleAdditionalTextChange}  />
-                    </div>
-                    <div className='flex flex-row'>
-                        <input className='mt-12 w-96 ml-8 mr-36 border-2 border-black rounded-xl' type='text' placeholder='Contact' value={Contact} onChange={handleContactChange} />
-                    </div>
-                </div>
-                <div className='flex flex-row justify-between'>
-                    <div className='flex flex-row'>
-                        <input className='mt-12 w-96 h-12 ml-36 border-2 border-black rounded-xl' type='text' placeholder='Name Of the Owner' value={Name} onChange={handleNameChange}  />
-                    </div>
-                    <div className='flex flex-col mt-6'>
-                       <input className='mt-6 w-96 mr-36 h-12 ml-36 border-2 border-black rounded-xl' type='text' placeholder='Quantity' value={Quantity} onChange={handleQuantityChange}  />
-                       <button className='mt-16 ml-36 w-36 bg-blue-500 text-white rounded-xl'>Submit</button>
-                    </div>
-                </div>
-                </form>
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
+
 export default PictureUploading2;
